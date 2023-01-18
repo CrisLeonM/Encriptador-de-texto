b8c520f3c1c4ab0d5724f02ab70a7f1bde7af9b8
Cristhian Leon <Cristhianleonem@gmail.com>
2023-01-18 12:14:04 -0500

    Agregando funsion que limpia los textareas

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
 function myFunction() {
     document.getElementById("div1").style.display = "none";
 } //Borra el texto y imagen de "ningun mensaje encontrado"//
+
+async function limpiar() {
+    //limpiar campo
+    document.getElementById("campo1").value = "";  
+}
