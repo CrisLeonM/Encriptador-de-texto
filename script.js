1c63738cd13a7c8a5e00a7759dae7d341071b11b
Cristhian Leon <Cristhianleonem@gmail.com>
2023-01-18 12:12:01 -0500

    Agregando funcion que esconde la imagen del: Ningun mensaje encontrado

---
+++ script.js
@@ -0,0 +1,4 @@
+//Borra el texto y imagen de "ningun mensaje encontrado"//
+function myFunction() {
+    document.getElementById("div1").style.display = "none";
+} //Borra el texto y imagen de "ningun mensaje encontrado"//
