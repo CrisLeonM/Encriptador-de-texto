ec45a6864c983ef25acca8591373be35512d8876
Cristhian Leon <Cristhianleonem@gmail.com>
2023-01-18 12:17:19 -0500

    Agregada funsion: Encriptar(reemplaza las vocales por otros valores)

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ async function limpiar() {
     //limpiar campo
     document.getElementById("campo1").value = "";  
 }
+
+function encriptar() {
+    // Obtiene el valor actual del textarea
+    var textareaValue = document.getElementById("campo1").value;
+    // Reemplaza las vocales en el valor del textarea
+    textareaValue = textareaValue.replace(/a/gi, "ap");
+    textareaValue = textareaValue.replace(/e/gi, "enter");
+    textareaValue = textareaValue.replace(/i/gi, "imes");
+    textareaValue = textareaValue.replace(/o/gi, "ober");
+    textareaValue = textareaValue.replace(/u/gi, "ufat");
+    // guarda el valor cambiado en una variable
+    var newTextareaValue = textareaValue;
+    
+    // Establece el nuevo valor del textarea
+    document.getElementById("campo1").value = textareaValue;
+  
+    // Establece el nuevo valor en el segundo textarea
+    document.getElementById("resultado").value = newTextareaValue;
+}
